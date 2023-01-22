b73b198ad8b12ac21c30f5b3f4e0bb47575dfc4a
Alvaro Dormundo <adormundo@gmail.com>
2023-01-21 23:34:13 -0300

    Componentes Layout e Titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <div className={`
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500 to-blue-600
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
     `}>
-      <h1 className="text-4xl">Hello World</h1>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
     </div>
   )
 }
