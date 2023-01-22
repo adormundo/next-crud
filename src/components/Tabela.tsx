f5a6c7217fa2747d5261bac9570a494a5b145ec3
Alvaro Dormundo <adormundo@gmail.com>
2023-01-22 11:03:48 -0300

    Componente Tabela

---
+++ src/components/Tabela.tsx
@@ -0,0 +1,80 @@
+import Cliente from "../core/Cliente"
+import { IconeEdicao, IconeLixo } from "./Icons"
+
+interface TabelaProps {
+  clientes: Cliente[]
+  clienteSelecionado?: (cliente: Cliente) => void
+  clienteExcluido?: (cliente: Cliente) => void
+}
+
+
+export default function Tabela(props: TabelaProps) {
+
+  const exibirAcoes = props.clienteExcluido || props.clienteSelecionado
+
+  function renderizarDados() {
+    return props.clientes?.map((cliente, i) => {
+      return (
+        <tr
+          key={cliente.id}
+          className={`${i % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'}`}
+        >
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+          {exibirAcoes ? renderizarAcoes(cliente) : false}
+        </tr>
+      )
+    })
+  }
+
+  function renderizarAcoes(cliente: Cliente) {
+    return (
+      <td className="flex justify-center">
+        {props.clienteSelecionado ? (
+          <button
+            className={`
+            flex justify-center items-center
+            text-green-600 p-2 m-1 rounded-full
+            hover:bg-purple-50
+          `}
+            onClick={() => props.clienteSelecionado?.(cliente)}
+          >
+            {IconeEdicao}
+          </button>
+        ) : false}
+
+
+        {props.clienteExcluido ? (
+          <button
+            className={`
+            flex justify-center items-center
+            text-red-500 p-2 m-1 rounded-full
+            hover:bg-purple-50
+          `}
+            onClick={() => props.clienteExcluido?.(cliente)}
+          >
+            {IconeLixo}
+          </button>
+        ) : false}
+      </td>
+    )
+  }
+
+  return (
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead className={`
+        text-gray-100
+        bg-gradient-to-r from from-purple-500 to-purple-800
+      `}>
+        <tr>
+          <th className="text-left p-4">Código</th>
+          <th className="text-left p-4">Nome</th>
+          <th className="text-left p-4">Idade</th>
+          {exibirAcoes ? (<th className="p-4">Ações</th>) : false}
+        </tr>
+      </thead>
+      <tbody>{renderizarDados()}</tbody>
+    </table>
+  )
+}
