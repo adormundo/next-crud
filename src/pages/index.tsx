5c0cd990b5c0beb2a63159aaf88be819521d1fc0
Alvaro Dormundo <adormundo@gmail.com>
2023-01-21 23:20:34 -0300

    project init 🎉

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 to-blue-600
+    `}>
+      <h1 className="text-4xl">Hello World</h1>
+    </div>
+  )
+}
