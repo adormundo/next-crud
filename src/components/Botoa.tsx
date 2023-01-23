9668965c61ee975d5a46988dd47912be0e059ba5
Alvaro Dormundo <adormundo@gmail.com>
2023-01-22 22:26:36 -0300

    Componente Botão

---
+++ src/components/Botoa.tsx
@@ -0,0 +1,20 @@
+interface BotaoProps {
+  className?: string
+  cor?: 'green' | 'blue' | 'gray'
+  children: any
+}
+
+export default function Botao(props: BotaoProps) {
+  const cor = props.cor ?? 'gray'
+
+  return (
+    <button
+      className={`
+        bg-gradient-to-r from-${cor}-400 to-${cor}-700
+        text-white px-4 py-2 rounded-md ${props.className}
+      `}
+    >
+      {props.children}
+    </button>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: [
     './src/**/*.tsx',
   ],
+  safelist: [
+    /^bg-/,
+    /^to-/,
+    /^from-/,
+  ],
   theme: {
     extend: {},
   },
